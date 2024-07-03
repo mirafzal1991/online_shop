97f715e243026e9a6484263d4bda7dd0c5366733
mirafzal <mirobitovmirafzalpython@gmail.com>
2024-07-04 02:13:14 +0500

    updated

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from shop.models import Category, Product, Order, Comment
+from shop.models import Category, Product, Order, Comment,User
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(Comment)
+admin.site.register(User)
